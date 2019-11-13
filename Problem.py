af19e34fde38029cf353529c4ccafebb1ed81588
nkfoss <fossx148@morris.umn.edu>
2019-11-13 15:51:59 -0600

    Add descriptive comments to problem.py

--- Problem.py
+++ Problem.py
@@ -2,13 +2,21 @@ from Node import node
 
 
 class problem:
-    boardSize = 0
+    # A problem is essentially a collection of islands, their x-y coordinates,
+    # and their required number of bridges. It initially has no islands.
     islands = []
+
+    # Since the size of the board depends completely on the size of the problem,
+    # we carry that information here. It is initially set to 0.
+    boardSize = 0
+
+    # We pass our desired islands and board size into the problem constructor.
     def __init__(self, islands, boardSize):
         self.boardSize = boardSize
         self.islands = islands
 
 
+# This is just a list of islands we used for our test problem (problem1)
 a = node(1, [0, 0], 2)
 b = node(1, [0, 3], 4)
 c = node(1, [0, 6], 3)
@@ -25,6 +33,7 @@ m = node(1, [6, 6], 2)
 
 problem1 = problem([a,b,c,d,e,f,g,h,i,j,k,l,m], 7)
 
+# An alternative test problem.
 a2 = node(1, [0, 0], 2)
 b2 = node(1, [4, 0], 1)
 c2 = node(1, [2, 0], 5)
