0b2a174835dbd927285e71093f8dc10ff7e10333
nkfoss <fossx148@morris.umn.edu>
2019-11-13 15:44:08 -0600

    Refactor Node and add descriptive comments
    Types is no longer a field, and but the type list still exists in the
    constructor. This was done b/c the constructor is the only scope it is used
    in.
    
    Also, since a node is assumed to empty by default, the fields were reset
    appropriately, and the 'if type == -1' check was removed from the constructor.

--- Node.py
+++ Node.py
@@ -1,36 +1,49 @@
 
 class node:
     # A node is a gridsquare that keeps track of many of the
-    # necessary properties of the puzzle
-    types = [-1, 0, 1]
+    # necessary properties of the puzzle.
+
+    # It has a set of x,y coordinates describing it's location on the board
+    location = []
+
+    # The node can either be empty (-1), a bridge (0), or an island (1).
+    # Nodes are intially empty, with no connected bridges or islands.
+    family = -1
+    typeString = "empty"
     connectedIslands = []
     connectedBridges = 0
-    # For islands, weight is the value
+
+    # IF the node becomes an island, 'weight' is the number of bridges
+    # required to complete the island. Empty and bridge nodes have weight = 0.
     weight = 0
-    # For bridges, bridges is the value
+
+    # This is the number of bridges in the node (doesn't need to be an island)
     bridges = 0
-    family = -1
-    location = []
+
     def __init__ (self, newType, location = [], weight = 0, connectedBridges = 0):
         self.location = location
         self.family = newType
         self.connectedBridges = connectedBridges
-        if newType not in self.types:
+
+        if newType not in [-1, 0, 1]:
             print("Type is not valid on", location)
-        # Node is an island
+
+        # During initialization, we do certain things depending on if the node is
+        # empty, a bridge, or an island.
+
+        # If it is empty.. we do nothing. The fields are set assuming it is empty.
+
+        # If the node is a bridge...
+        if type == 0:
+            self.family = 0
+            self.typeString = "bridge"
+
+        # If node is an island, set the weight
         if self.family == 1:
             self.weight = weight
             self.typeString = "island"
-        if type == 0:
-            self.family = 0
-            self.weight = 0
-            self.typeString = "bridge"
-        if type == -1:
-            self.weight = 0
-            self.typeString = "empty"
 
-
-
+    # It is useful to know when an island has all it's required bridges.
     def checkFull(self):
         # Returns True if full
         return((self.weight - self.connectedBridges) <= 0)
