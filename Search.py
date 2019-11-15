7496dd2e7afbf785113628ab667d4d534deb80a5
nkfoss <fossx148@morris.umn.edu>
2019-11-15 15:39:38 -0600

    Add descriptive comments for methods and dividers for readability
    Roen: Please read the 'NOTE' comments and respond.
    (use ctrl+f to find them).
    
    No actual refactoring here. Just comments/organization of the code.
    
    Variable "movestotal" was moved away from generateMoves()
    and put into the 'fields' section.
    
    Dividers were added between individual methods, as well as between
    more comprehensive sections (fields, methods, running the search).

--- Search.py
+++ Search.py
@@ -3,6 +3,10 @@ from Problem import problem1, problem2
 from Node import *
 import copy
 
+#==============================================================================#
+#======  Fields  ==============================================================#
+#==============================================================================#
+
 problem = problem1
 
 currentBoard = 0
@@ -12,30 +16,69 @@ visited = []
 
 totalValues = 0
 
+movesTotal = 1
+
+#=============================================================================#
+#=======  Methods  ===========================================================#
+#=============================================================================#
+
+# This is the first function called during search. This sets up the intial board
+# state as defined by the problem.
 def initialize():
     global currentBoard
-    n = problem.boardSize
-    initialBoard = board(n, 0, 0)
+
+    # Make the first board-state, with the appropriate size and heuristic = 0
+    initialBoard = board(problem.boardSize, 0)
+
+    # For each island in the problem, set the board's grid to be that island at
+    # the appropriate location, and add that island to the board's island list.
     for island in problem.islands:
         x = island.location[0]
         y = island.location[1]
         initialBoard.grid[x][y] = island
         initialBoard.islands.add(island)
+
+    # Update the frontier and visited list with a DEEPCOPY of the board
+
+    # !!! NOTE !!!
+    # I believe there should be some check done so we don't try generating children
+    # from a board-state that has already been visited. Right now, the initial state
+    # is in BOTH the frontier and visited list. (nkfoss)
+
     currentBoard = copy.deepcopy(initialBoard)
     frontier.append(currentBoard)
     visited.append(currentBoard)
 
+#==============================================================================#
+
+# This just prints every island's location and number of bridges left to connect
 def printIslands(board):
     for island in board.islands:
-        print(island.location, island.weight-island.connectedBridges)
+        print(island.location, island.weight - island.connectedBridges)
 
+#==============================================================================#
+
+# Sum every island's weight, and assign to totalValues
 def calculateValues(board):
     global totalValues
     for island in board.islands:
         totalValues = totalValues + island.weight
 
+#==============================================================================#
 
+# Take a list of islands, copy each island to a list, and then return the list.
 def copyNodes(islands):
+
+# !!! NOTE !!!
+
+# I think this function is redundant...
+# makeChildren(board) does this...
+# > boardCopy = copy.deepcopy(board)
+# > boardCopy.islands = copyNodes(boardCopy.islands)
+
+# so, we are just reassigning a list of islands from one object to the same
+# object? Is that correct? (nkfoss)
+
     islandList = []
     for island in islands:
         a = node(1, island.location, island.weight, island.connectedBridges)
@@ -44,19 +87,39 @@ def copyNodes(islands):
         islandList.append(a)
     return islandList
 
-# Compares all of the bridges in the current board and each board in the frontier. It will only get added to the frontier if it doesn't exist already in the frontier.
-def compareBoards(boardCopy, boardList):
-    for board in boardList:
+#==============================================================================#
+
+# Used by makeChildren(board, visited)
+# Returns a boolean indicating that the board is not visited yet.
+# Compares all of the bridges in the current board and each board in the
+# frontier. It will only get added to the frontier if it doesn't exist already
+# in the frontier.
+def compareBoards(boardCopy, visited):
+
+    # For each board
+    for board in visited:
         for col in board.grid:
             for cell in col:
+
+
                 bCell = boardCopy.grid[cell.location[0]][cell.location[1]]
-                # If the same node has the same amount of bridges
+
+                # If the node in board.copy is not the same as the node in board
+                # Essentially to see if a node was turned into a 'bridge' node
                 if bCell.family != cell.family:
                     return False
+
+                # If they are the same family, see if the bridges are the same.
+                # This includes islands AND bridge nodes (which can contain up
+                # to 2 bridges)
                 if bCell.bridges != cell.bridges:
                     return False
+
+
     return True
-                    
+
+#==============================================================================#
+
 # checkRow and Col can return a list of the cells to be turned into bridges.
 # This makes it easy to make bridges along the path that we already iterate across.
 def checkRow(row, ay, by, aLoc, bLoc, board):
@@ -76,6 +139,8 @@ def checkRow(row, ay, by, aLoc, bLoc, board):
                 return False
         return toBeBridges
 
+#==============================================================================#
+
 def checkCol(col, ax, bx, aLoc, bLoc, board):
     toBeBridges = []
     if ax < bx:
@@ -92,6 +157,8 @@ def checkCol(col, ax, bx, aLoc, bLoc, board):
             if cCell.family == 1 and cCell.location != aLoc:
                 return False
         return toBeBridges
+
+#==============================================================================#
 
 def populateAdjacent(nodeA, board):
     ax = nodeA.location[0]
@@ -126,7 +193,8 @@ def populateAdjacent(nodeA, board):
                     adjacents.append(island)
             break
     return adjacents
-        
+
+#==============================================================================#
 
 def checkAdjacent(nodeA, nodeB, board):
     aLoc = nodeA.location
@@ -142,7 +210,9 @@ def checkAdjacent(nodeA, nodeB, board):
     # Same Row
     if ay == by:
         print("same row")
-        return(checkCol(ay, ax, bx, aLoc, bLoc, board))       
+        return(checkCol(ay, ax, bx, aLoc, bLoc, board))
+
+#==============================================================================#
 
 def checkFullPair(nodeA, nodeB):
     a = nodeA
@@ -150,6 +220,8 @@ def checkFullPair(nodeA, nodeB):
     aFull = nodeA.checkFull()
     bFull = nodeB.checkFull()
     return aFull or bFull
+
+#==============================================================================#
 
 def connect(nodeA, nodeB, board):
     toBeBridges = checkAdjacent(nodeA, nodeB, board)
@@ -165,13 +237,16 @@ def connect(nodeA, nodeB, board):
         nodeA.connectedIslands.append(nodeB.location)
         nodeB.connectedIslands.append(nodeA.location)
 
+#==============================================================================#
+
 def calculateHeuristic(board):
     totalBridges = 0
     for island in board.islands:
         totalBridges = totalBridges + island.connectedBridges
     return (totalValues - totalBridges)
 
-movesTotal = 1
+#==============================================================================#
+
 # Generate possible moves given a boardstate
 # This means only adjacent nodes that aren't full
 def generateMoves(board):
@@ -189,11 +264,13 @@ def generateMoves(board):
     movesTotal = len(moves)
     return moves
 
+#==============================================================================#
+
 def makeChildren(board):
     global finished
     for move in (generateMoves(board)):
         boardCopy = copy.deepcopy(board)
-        boardCopy.islands = copyNodes(boardCopy.islands)
+        boardCopy.islands = copyNodes(boardCopy.islands) # Is this redundant...?
         mList = list(move)
         a = mList.pop()
         b = mList.pop()
@@ -212,9 +289,11 @@ def makeChildren(board):
 
         # Add new board to the frontier if it has not already been explored.
         x = compareBoards(boardCopy, visited)
-        
+
         if not x:
             frontier.append(boardCopy)
+
+#==============================================================================#
 
 def checkFinished(board):
     bridges = 0
@@ -224,7 +303,7 @@ def checkFinished(board):
         weight = island.weight + weight
     return bridges == weight
 
-
+#==============================================================================#
 
 def search(runs):
     global lastBoard
@@ -241,13 +320,16 @@ def search(runs):
             currentBoard = frontier[0]
             printIslands(currentBoard)
 
+#==============================================================================#
+#======  Running the Search  ==================================================#
+#==============================================================================#
 
 initialize()
 lastBoard = frontier[0]
 finished = False
 calculateValues(currentBoard)
 currentBoard.heuristic = calculateHeuristic(currentBoard)
-search(10000)
+search(100)
 # printIslands(currentBoard)
 
 
